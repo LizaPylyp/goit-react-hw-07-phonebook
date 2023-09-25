091ee00c9ff9a9b4b56702c2e67ccefd096dc655
Liza <lizkapylypchuk123456@gmail.com>
2023-09-25 11:22:35 +0300

    Update actions.js

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -13,7 +13,17 @@ export const fetchContacts = createAsyncThunk(
       }
     }
 );
-
+export const deleteContact = createAsyncThunk(
+  'contacts/deleteContact',
+async (contactId, thunkAPI) => {
+  try {
+    const response = await axios.delete(`/contacts/${contactId}`);
+    return response.data;
+  } catch (e) {
+    return thunkAPI.rejectWithValue(e.message);
+  }
+}
+);
 export const addContact = createAsyncThunk(
     'contacts/addContact',
   async ({ name, phone }, thunkAPI) => {
@@ -29,14 +39,3 @@ export const addContact = createAsyncThunk(
   }
 );
 
-export const deleteContact = createAsyncThunk(
-    'contacts/deleteContact',
-  async (contactId, thunkAPI) => {
-    try {
-      const response = await axios.delete(`/contacts/${contactId}`);
-      return response.data;
-    } catch (e) {
-      return thunkAPI.rejectWithValue(e.message);
-    }
-  }
-);
